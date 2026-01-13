5eb2b56138ec00008db356cf63ec0a4899c31eaf
RjGutierrezz <rbg002@uark.edu>
2026-01-12 23:43:44 -0800

    added some more themse

--- lua/sethy/plugins/colorscheme.lua
+++ lua/sethy/plugins/colorscheme.lua
@@ -253,4 +253,128 @@ return {
             -- })
         end,
     },
+    -- NOTE: CATPPUSINN
+    {
+        "catppuccin/nvim",
+        config = function()
+          require("catppuccin").setup({
+              flavour = "auto", -- latte, frappe, macchiato, mocha
+              background = { -- :h background
+                  light = "latte",
+                  dark = "mocha",
+              },
+              transparent_background = true, -- disables setting the background color.
+              float = {
+                  transparent = true, -- enable transparent floating windows
+                  solid = false, -- use solid styling for floating windows, see |winborder|
+              },
+              show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+              term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+              dim_inactive = {
+                  enabled = false, -- dims the background color of inactive window
+                  shade = "dark",
+                  percentage = 0.15, -- percentage of the shade to apply to the inactive window
+              },
+              no_italic = false, -- Force no italic
+              no_bold = false, -- Force no bold
+              no_underline = false, -- Force no underline
+              styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+                  comments = { "italic" }, -- Change the style of comments
+                  conditionals = { "italic" },
+                  loops = {},
+                  functions = {},
+                  keywords = {},
+                  strings = {},
+                  variables = {},
+                  numbers = {},
+                  booleans = {},
+                  properties = {},
+                  types = {},
+                  operators = {},
+                  -- miscs = {}, -- Uncomment to turn off hard-coded styles
+              },
+              lsp_styles = { -- Handles the style of specific lsp hl groups (see `:h lsp-highlight`).
+                  virtual_text = {
+                      errors = { "italic" },
+                      hints = { "italic" },
+                      warnings = { "italic" },
+                      information = { "italic" },
+                      ok = { "italic" },
+                  },
+                  underlines = {
+                      errors = { "underline" },
+                      hints = { "underline" },
+                      warnings = { "underline" },
+                      information = { "underline" },
+                      ok = { "underline" },
+                  },
+                  inlay_hints = {
+                      background = true,
+                  },
+              },
+              color_overrides = {},
+              custom_highlights = {},
+              default_integrations = true,
+              auto_integrations = false,
+              integrations = {
+                  cmp = true,
+                  gitsigns = true,
+                  nvimtree = true,
+                  notify = false,
+                  mini = {
+                      enabled = true,
+                      indentscope_color = "",
+                  },
+                  -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+              },
+          })
+        end,
+    },
+    -- NOTE: One Dark
+    {
+      "navarasu/onedark.nvim",
+      -- priority = 1000,
+      config = function()
+      -- Lua
+        require('onedark').setup  {
+            -- Main options --
+            style = 'dark', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+            transparent = true,  -- Show/hide background
+            term_colors = true, -- Change terminal color as per the selected theme style
+            ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
+            cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
+
+            -- toggle theme style ---
+            toggle_style_key = nil, -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
+            toggle_style_list = {'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light'}, -- List of styles to toggle between
+
+            -- Change code style ---
+            -- Options are italic, bold, underline, none
+            -- You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'
+            code_style = {
+                comments = 'italic',
+                keywords = 'none',
+                functions = 'none',
+                strings = 'none',
+                variables = 'none'
+            },
+
+            -- Lualine options --
+            lualine = {
+                transparent = true, -- lualine center bar transparency
+            },
+
+            -- Custom Highlights --
+            colors = {}, -- Override default colors
+            highlights = {}, -- Override highlight groups
+
+            -- Plugins Config --
+            diagnostics = {
+                darker = true, -- darker colors for diagnostic
+                undercurl = true,   -- use undercurl instead of underline for diagnostics
+                background = true,    -- use background color for virtual text
+            },
+        }
+      end,
+    }
 }
