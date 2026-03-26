46163436d06546911f3927358cb5013186ad0457
RjGutierrezz <rbg002@uark.edu>
2026-03-25 20:27:17 -0700

    update

--- lua/sethy/plugins/colorscheme.lua
+++ lua/sethy/plugins/colorscheme.lua
@@ -197,15 +197,15 @@ return {
 		-- priority = 1000,
 		config = function()
 			local transparent = true
-			local bg = "#011628"
-			local bg_dark = "#011423"
-			local bg_highlight = "#143652"
-			local bg_search = "#0A64AC"
-			local bg_visual = "#275378"
-			local fg = "#CBE0F0"
-			local fg_dark = "#B4D0E9"
-			local fg_gutter = "#627E97"
-			local border = "#547998"
+			-- local bg = "#011628"
+			-- local bg_dark = "#011423"
+			-- local bg_highlight = "#143652"
+			-- local bg_search = "#0A64AC"
+			-- local bg_visual = "#275378"
+			-- local fg = "#CBE0F0"
+			-- local fg_dark = "#B4D0E9"
+			-- local fg_gutter = "#627E97"
+			-- local border = "#547998"
 
 			require("tokyonight").setup({
 				style = "night",
@@ -217,23 +217,23 @@ return {
 					sidebars = transparent and "transparent" or "dark",
 					floats = transparent and "transparent" or "dark",
 				},
-				on_colors = function(colors)
-					colors.bg = transparent and colors.none or bg
-					colors.bg_dark = transparent and colors.none or bg_dark
-					colors.bg_float = transparent and colors.none or bg_dark
-					colors.bg_highlight = bg_highlight
-					colors.bg_popup = bg_dark
-					colors.bg_search = bg_search
-					colors.bg_sidebar = transparent and colors.none or bg_dark
-					colors.bg_statusline = transparent and colors.none or bg_dark
-					colors.bg_visual = bg_visual
-					colors.border = border
-					colors.fg = fg
-					colors.fg_dark = fg_dark
-					colors.fg_float = fg
-					colors.fg_gutter = fg_gutter
-					colors.fg_sidebar = fg_dark
-				end,
+				-- on_colors = function(colors)
+				-- 	colors.bg = transparent and colors.none or bg
+				-- 	colors.bg_dark = transparent and colors.none or bg_dark
+				-- 	colors.bg_float = transparent and colors.none or bg_dark
+				-- 	colors.bg_highlight = bg_highlight
+				-- 	colors.bg_popup = bg_dark
+				-- 	colors.bg_search = bg_search
+				-- 	colors.bg_sidebar = transparent and colors.none or bg_dark
+				-- 	colors.bg_statusline = transparent and colors.none or bg_dark
+				-- 	colors.bg_visual = bg_visual
+				-- 	colors.border = border
+				-- 	colors.fg = fg
+				-- 	colors.fg_dark = fg_dark
+				-- 	colors.fg_float = fg
+				-- 	colors.fg_gutter = fg_gutter
+				-- 	colors.fg_sidebar = fg_dark
+				-- end,
 			})
 			-- vim.cmd("colorscheme tokyonight")
 			-- NOTE: Auto switch to tokyonight for markdown files only
@@ -281,7 +281,7 @@ return {
 				no_underline = false, -- Force no underline
 				styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
 					comments = { "italic" }, -- Change the style of comments
-					conditionals = { "italic" },
+					conditionals = {},
 					loops = {},
 					functions = {},
 					keywords = {},
